760b386237a2d2e3c7cd8e1357886bb35fbf591b
Abdullah-Al-Kawser Jamaly <56441802+kawseribnjamal@users.noreply.github.com>
2025-07-12 18:23:33 +0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,18 +1,17 @@
 // firebase.js
-
 const firebaseConfig = {
   apiKey: "AIzaSyCE3veqwiTfCf5T4FEmNWPF2XrVxy0hFa8",
   authDomain: "kawseribnjamal-99d0f.firebaseapp.com",
   projectId: "kawseribnjamal-99d0f",
   storageBucket: "kawseribnjamal-99d0f.appspot.com",
   messagingSenderId: "255855799684",
-  appId: "1:255855799684:web:5185b604935799684881c1d1"
+  appId: "1:255855799684:web:5185b6049351e4a881c1d1",
+  measurementId: "G-X8E7HD9CK"
 };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-// Initialize Firebase services
+// Initialize services
 const auth = firebase.auth();
 const db = firebase.firestore();
-const storage = firebase.storage();
